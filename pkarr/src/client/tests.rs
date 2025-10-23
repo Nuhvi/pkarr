7dcc9dde118ed937e9726d276c2957d42a686a4f
nazeh <nuh@nuh.dev>
2025-10-23 20:37:35 +0300

    fix: increase request timeout in local tests

--- pkarr/src/client/tests.rs
+++ pkarr/src/client/tests.rs
@@ -36,7 +36,7 @@ pub(crate) fn builder(
     if std::env::var("CI").is_ok() {
         builder.request_timeout(Duration::from_millis(1000));
     } else {
-        builder.request_timeout(Duration::from_millis(200));
+        builder.request_timeout(Duration::from_millis(400));
     }
 
     match networks {
